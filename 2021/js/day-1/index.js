27a0e30af699686e1172178f88396ba3e901c761
Linus Thiel <linus.thiel@bonniernews.se>
2021-12-02 16:39:14 +0100

    Formatting: Add eslint + prettier, formatting day-1
    Also, adds JSDoc.

--- 2021/js/day-1/index.js
+++ 2021/js/day-1/index.js
@@ -1,14 +1,9 @@
-const {
-    part1,
-    part2,
-} = require('./solution');
+const { part1, part2 } = require('./solution');
 
 /**
- * @tscheck
- * 
  * The solution for Advent of Code 2021, day 1. The following is the
  * description of the situation in which we find ourselves:
- * 
+ *
  * As the submarine drops below the surface of the ocean, it automatically
  * performs a sonar sweep of the nearby sea floor. On a small screen, the
  * sonar sweep report (your puzzle input) appears: each line is a measurement
@@ -16,20 +11,20 @@ const {
  * submarine.
  */
 function main() {
-    let input = [];
-    process.stdin
-      .on('data', (chunk) => input.push(chunk))
-      .on('end', () => {
-          const numbers = input
-            .join('')
-            .split(/\s+/)
-            .map(s => s.trim())
-            .filter(Boolean)
-            .map(s => parseInt(s, 10));
+  let input = [];
+  process.stdin
+    .on('data', (chunk) => input.push(chunk))
+    .on('end', () => {
+      const numbers = input
+        .join('')
+        .split(/\s+/)
+        .map((s) => s.trim())
+        .filter(Boolean)
+        .map((s) => parseInt(s, 10));
 
-            console.log('part 1', part1(numbers));
-            console.log('part 2', part2(numbers));
-      });
+      console.log('part 1:', part1(numbers));
+      console.log('part 2:', part2(numbers));
+    });
 }
 
 main(process.argv.slice(2));
--- 2021/js/day-1/solution.js
+++ 2021/js/day-1/solution.js
@@ -9,7 +9,7 @@
  * To do this, count **the number of times a depth measurement increases** from
  * the previous measurement. (There is no measurement before the first
  * measurement.) In the example above, the changes are as follows:
- * 
+ *
  * ```text
  * 199 (N/A - no previous measurement)
  * 200 (increased)
@@ -22,17 +22,17 @@
  * 260 (decreased)
  * 263 (increased)
  * ```
- * 
+ *
  * In this example, there are `7` measurements that are larger than the previous
  * measurement.
- * 
+ *
  * **How many measurements are larger than the previous measurement?**
- * 
- * @param {number[]} numbers
- * 
+ *
+ * @param {number[]} numbers The depth measurements to process
+ *
  * @returns {number} The number of measurements that are larger than the
  * previous measurement
- * 
+ *
  * @example part1([
  *   199,
  *   200,
@@ -59,11 +59,12 @@ function part1 (numbers) {
 /**
  * Considering every single measurement isn't as useful as you expected:
  * there's just too much noise in the data.
- * 
+ *
  * Instead, consider sums of a **three-measurement sliding window**. Again
  * considering the above example:
- * 
- * ```text
+ *
+ * ```text // eslint-disable-next-line prettier/prettier
+
  * 199  A      
  * 200  A B    
  * 208  A B C  
@@ -75,20 +76,20 @@ function part1 (numbers) {
  * 260      G H
  * 263        H
  * ```
- * 
+ *
  * Start by comparing the first and second three-measurement windows. The
  * measurements in the first window are marked A (199, 200, 208); their sum is
  * 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its
  * sum is 618. The sum of measurements in the second window is larger than the
  * sum of the first, so this first comparison **increased**.
- * 
+ *
  * Your goal now is to count **the number of times the sum of measurements in
  * this sliding window increases** from the previous sum. So, compare A with B,
  * then compare B with C, then C with D, and so on. Stop when there aren't
  * enough measurements left to create a new three-measurement sum.
- * 
+ *
  * In the above example, the sum of each three-measurement window is as follows:
- * 
+ *
  * ```text
  * A: 607 (N/A - no previous sum)
  * B: 618 (increased)
@@ -99,17 +100,17 @@ function part1 (numbers) {
  * G: 769 (increased)
  * H: 792 (increased)
  * ```
- * 
+ *
  * In this example, there are `5` sums that are larger than the previous sum.
- * 
+ *
  * Consider sums of a three-measurement sliding window. **How many sums are
  * larger than the previous sum?**
- * 
- * @param {number[]} numbers
- * 
+ *
+ * @param {number[]} numbers The depth measurements to process
+ *
  * @returns {number} The number of sums of three-measurement sliding windows
- * that are larger than the previous sum
- * 
+ *     that are larger than the previous sum
+ *
  * @example part2([
  *   199,
  *   200,
@@ -145,6 +146,6 @@ function part2 (numbers) {
 }
 
 module.exports = {
-    part1,
-    part2,
+  part1,
+  part2,
 };
