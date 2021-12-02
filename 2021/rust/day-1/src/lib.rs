8530546e79db1c74f3262365c95ae46e3e25b1db
Linus Thiel <linus.thiel@bonniernews.se>
2021-12-02 09:59:01 +0100

    Formatting

--- 2021/rust/day-1/src/lib.rs
+++ 2021/rust/day-1/src/lib.rs
@@ -1,6 +1,6 @@
 //! The solution for Advent of Code 2021, day 1. The following is the
 //! description of the situation in which we find ourselves:
-//! 
+//!
 //! As the submarine drops below the surface of the ocean, it automatically
 //! performs a sonar sweep of the nearby sea floor. On a small screen, the
 //! sonar sweep report (your puzzle input) appears: each line is a measurement
@@ -15,7 +15,7 @@
 /// To do this, count **the number of times a depth measurement increases** from
 /// the previous measurement. (There is no measurement before the first
 /// measurement.) In the example above, the changes are as follows:
-/// 
+///
 /// ```text
 /// 199 (N/A - no previous measurement)
 /// 200 (increased)
@@ -28,12 +28,12 @@
 /// 260 (decreased)
 /// 263 (increased)
 /// ```
-/// 
+///
 /// In this example, there are `7` measurements that are larger than the previous
 /// measurement.
-/// 
+///
 /// **How many measurements are larger than the previous measurement?**
-/// 
+///
 /// ```
 /// let input = [
 ///   199,
@@ -65,10 +65,10 @@ pub fn part1(numbers: &[usize]) -> usize {
 
 /// Considering every single measurement isn't as useful as you expected:
 /// there's just too much noise in the data.
-/// 
+///
 /// Instead, consider sums of a **three-measurement sliding window**. Again
 /// considering the above example:
-/// 
+///
 /// ```text
 /// 199  A      
 /// 200  A B    
@@ -81,20 +81,20 @@ pub fn part1(numbers: &[usize]) -> usize {
 /// 260      G H
 /// 263        H
 /// ```
-/// 
+///
 /// Start by comparing the first and second three-measurement windows. The
 /// measurements in the first window are marked A (199, 200, 208); their sum is
 /// 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its
 /// sum is 618. The sum of measurements in the second window is larger than the
 /// sum of the first, so this first comparison **increased**.
-/// 
+///
 /// Your goal now is to count **the number of times the sum of measurements in
 /// this sliding window increases** from the previous sum. So, compare A with B,
 /// then compare B with C, then C with D, and so on. Stop when there aren't
 /// enough measurements left to create a new three-measurement sum.
-/// 
+///
 /// In the above example, the sum of each three-measurement window is as follows:
-/// 
+///
 /// ```text
 /// A: 607 (N/A - no previous sum)
 /// B: 618 (increased)
@@ -105,12 +105,12 @@ pub fn part1(numbers: &[usize]) -> usize {
 /// G: 769 (increased)
 /// H: 792 (increased)
 /// ```
-/// 
+///
 /// In this example, there are `5` sums that are larger than the previous sum.
-/// 
+///
 /// Consider sums of a three-measurement sliding window. **How many sums are
 /// larger than the previous sum?**
-/// 
+///
 /// ```
 /// let input = [
 ///   199,
