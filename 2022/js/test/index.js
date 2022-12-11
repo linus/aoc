197a8e1d6283ab24bcd17a9520f32bcb2a12aa30
Linus Thiel <linus.thiel@bonniernews.se>
2022-12-11 11:15:55 +0100

    Add @linus/testy, tests, other deps

---
+++ 2022/js/test/index.js
@@ -0,0 +1,37 @@
+import { testy as test } from '@linus/testy';
+
+describe('AoC 2022', () => {
+  describe('Days', () => {
+    describe('Day 1', () => test('day-01/index.js'));
+    describe('Day 2', () => {
+      test('day-02/index.js');
+      test('day-02/lib.js');
+    });
+    describe('Day 3', () => {
+      test('day-03/index.js');
+      test('day-03/lib.js');
+    });
+    describe('Day 4', () => {
+      test('day-04/index.js');
+      test('day-04/lib.js');
+    });
+    describe('Day 5', () => test('day-05/index.js'));
+    describe('Day 6', () => {
+      test('day-06/index.js');
+      test('day-06/lib.js');
+    });
+    describe('Day 7', () => {
+      test('day-07/index.js');
+      test('day-07/lib.js');
+    });
+
+    describe('Day 8', () => {
+      test('day-08/index.js');
+      test('day-08/lib.js');
+    });
+  });
+
+  describe('Libraries', () => {
+    describe('Utils', () => test('utils.js'));
+  });
+});
