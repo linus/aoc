1ff2e30a2adbff2bd7d5b91919ef50090e211cad
Linus Thiel <linus.thiel@bonniernews.se>
2024-12-01 20:46:40 +0100

    Refactoring

--- 2024/js/day-01/index.js
+++ 2024/js/day-01/index.js
@@ -16,25 +16,32 @@
  * }
  */
 export function solution(input) {
-  const lines = input.trim().split('\n');
-  const left = [],
-    right = [];
-  for (const line of lines) {
-    const [l, r] = line.split(/\s+/);
-    left.push(Number(l));
-    right.push(Number(r));
-  }
-  left.sort();
-  right.sort();
+  /** @type {number[][]} */
+  const [left, right] = input
+    .trim()
+    .split('\n')
+    .map((line) => line.split(/\s+/).map(Number))
+    .reduce(
+      ([left, right], [l, r]) => {
+        left.push(l);
+        right.push(r);
+        return [left, right];
+      },
+      /** @type {number[][]} */ [[], []]
+    )
+    .map((arr) => arr.sort());
 
   const diffs = left.map((l, i) => Math.abs(l - right[i]));
 
-  const freqs = right.reduce((acc, num) => {
-    acc[num] = (acc[num] || 0) + 1;
-    return acc;
-  }, {});
+  const freqs = right.reduce(
+    (freqs, num) => {
+      freqs[num] = (freqs[num] || 0) + 1;
+      return freqs;
+    },
+    /** @type {Record<number, number>} */ {}
+  );
 
-  const similarities = left.map((l) => l * freqs[l] || 0);
+  const similarities = left.map((l) => l * (freqs[l] ?? 0));
 
   return {
     part1: diffs.reduce((a, b) => a + b),
