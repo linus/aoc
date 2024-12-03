5feaee72cab0eff313f97b7347f499b73a6d267e
Linus Thiel <linus.thiel@bonniernews.se>
2024-12-03 16:02:54 +0100

    Fix comment, no AoC text should be spread

--- 2024/js/index.js
+++ 2024/js/index.js
@@ -1,22 +1,7 @@
 import { createReadStream } from 'fs';
 /**
  *
- * --- Day 1: Historian Hysteria ---
- *
- * The Chief Historian is always present for the big Christmas sleigh launch,
- * but nobody has seen him in months! Last anyone heard, he was visiting
- * locations that are historically significant to the North Pole; a group of
- * Senior Historians has asked you to accompany them as they check the places
- * they think he was most likely to visit.
- *
- * As each location is checked, they will mark it on their list with a _star_.
- * They figure the Chief Historian must be in one of the first fifty places
- * they'll look, so in order to save Christmas, you need to help them get _fifty
- * stars_ on their list before Santa takes off on December 25th.
- *
- * Collect stars by solving puzzles. Two puzzles will be made available on each
- * day in the Advent calendar; the second puzzle is unlocked when you complete
- * the first. Each puzzle grants _one star_. Good luck!
+ * 🕯🕯🕯🕯 Advent of Code 2024 🕯🕯🕯🕯
  *
  * @param {string} day
  * @param {string=} inputFile
