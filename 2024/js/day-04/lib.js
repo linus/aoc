dc5ea0eba9a0c3f6c31e4121707c2845514606e0
Linus Thiel <linus.thiel@bonniernews.se>
2024-12-04 17:07:52 +0100

    Moar formatting

--- 2024/js/day-04/lib.js
+++ 2024/js/day-04/lib.js
@@ -31,7 +31,10 @@ export function search(haystack, needle) {
     cols: for (let j = 0; j < cols - m + 1; j++) {
       for (let k = 0; k < n; k++) {
         for (let l = 0; l < m; l++) {
-          if (needle[k][l] !== undefined && needle[k][l] !== haystack[i + k][j + l]) {
+          if (
+            needle[k][l] !== undefined &&
+            needle[k][l] !== haystack[i + k][j + l]
+          ) {
             continue cols;
           }
         }
